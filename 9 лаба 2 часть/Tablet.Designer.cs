a0289133b38d0fb64ba42ffb27199a8652ee4526
Пользователь <@DESKTOP-I15JQK7>
2023-12-13 18:39:04 +0300

    Добавьте файлы проекта.

---
+++ 9 лаба 2 часть/Tablet.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace _9_лаба_2_часть
+{
+
+    partial class Tablet
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbAdd = new System.Windows.Forms.ToolStripButton();
+            this.tsbDelete = new System.Windows.Forms.ToolStripButton();
+            this.tsbPrev = new System.Windows.Forms.ToolStripButton();
+            this.tsbNext = new System.Windows.Forms.ToolStripButton();
+            this.tslCount = new System.Windows.Forms.ToolStripLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbModel = new System.Windows.Forms.TextBox();
+            this.tbDiagonal = new System.Windows.Forms.TextBox();
+            this.tbOS = new System.Windows.Forms.TextBox();
+            this.tbMemory = new System.Windows.Forms.TextBox();
+            this.tbStorage = new System.Windows.Forms.TextBox();
+            this.tbCPU = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbAdd,
+            this.tsbDelete,
+            this.tsbPrev,
+            this.tsbNext,
+            this.tslCount});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(364, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbAdd
+            // 
+            this.tsbAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbAdd.Image = global::_9_лаба_2_часть.Properties.Resources.ImageAddButton;
+            this.tsbAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbAdd.Name = "tsbAdd";
+            this.tsbAdd.Size = new System.Drawing.Size(23, 22);
+            this.tsbAdd.Text = "Добавить запись";
+            this.tsbAdd.Click += new System.EventHandler(this.tsbAdd_Click);
+            // 
+            // tsbDelete
+            // 
+            this.tsbDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbDelete.Image = global::_9_лаба_2_часть.Properties.Resources.Image1;
+            this.tsbDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbDelete.Name = "tsbDelete";
+            this.tsbDelete.Size = new System.Drawing.Size(23, 22);
+            this.tsbDelete.Text = "Удалить запись";
+            this.tsbDelete.Click += new System.EventHandler(this.tsbDelete_Click);
+            // 
+            // tsbPrev
+            // 
+            this.tsbPrev.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbPrev.Image = global::_9_лаба_2_часть.Properties.Resources.Image3;
+            this.tsbPrev.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbPrev.Name = "tsbPrev";
+            this.tsbPrev.Size = new System.Drawing.Size(23, 22);
+            this.tsbPrev.Text = "Предыдущая запись";
+            this.tsbPrev.Click += new System.EventHandler(this.tsbPrev_Click);
+            // 
+            // tsbNext
+            // 
+            this.tsbNext.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbNext.Image = global::_9_лаба_2_часть.Properties.Resources.Image2;
+            this.tsbNext.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNext.Name = "tsbNext";
+            this.tsbNext.Size = new System.Drawing.Size(23, 22);
+            this.tsbNext.Text = "Следующая запись";
+            this.tsbNext.Click += new System.EventHandler(this.tsbNext_Click);
+            // 
+            // tslCount
+            // 
+            this.tslCount.Name = "tslCount";
+            this.tslCount.Size = new System.Drawing.Size(92, 22);
+            this.tslCount.Text = "кол-во записей";
+            this.tslCount.LocationChanged += new System.EventHandler(this.tslCount_LocationChanged);
+            this.tslCount.Paint += new System.Windows.Forms.PaintEventHandler(this.tslCount_Paint);
+            this.tslCount.TextChanged += new System.EventHandler(this.tslCount_TextChanged);
+            this.tslCount.VisibleChanged += new System.EventHandler(this.tslCount_VisibleChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "модель";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "диагональ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ос";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "опер. память";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "встр. память";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "процессор";
+            // 
+            // tbModel
+            // 
+            this.tbModel.Location = new System.Drawing.Point(150, 38);
+            this.tbModel.Name = "tbModel";
+            this.tbModel.Size = new System.Drawing.Size(164, 20);
+            this.tbModel.TabIndex = 7;
+            // 
+            // tbDiagonal
+            // 
+            this.tbDiagonal.Location = new System.Drawing.Point(150, 69);
+            this.tbDiagonal.Name = "tbDiagonal";
+            this.tbDiagonal.Size = new System.Drawing.Size(164, 20);
+            this.tbDiagonal.TabIndex = 8;
+            // 
+            // tbOS
+            // 
+            this.tbOS.Location = new System.Drawing.Point(150, 103);
+            this.tbOS.Name = "tbOS";
+            this.tbOS.Size = new System.Drawing.Size(164, 20);
+            this.tbOS.TabIndex = 9;
+            // 
+            // tbMemory
+            // 
+            this.tbMemory.Location = new System.Drawing.Point(150, 134);
+            this.tbMemory.Name = "tbMemory";
+            this.tbMemory.Size = new System.Drawing.Size(164, 20);
+            this.tbMemory.TabIndex = 10;
+            // 
+            // tbStorage
+            // 
+            this.tbStorage.Location = new System.Drawing.Point(150, 163);
+            this.tbStorage.Name = "tbStorage";
+            this.tbStorage.Size = new System.Drawing.Size(164, 20);
+            this.tbStorage.TabIndex = 11;
+            // 
+            // tbCPU
+            // 
+            this.tbCPU.Location = new System.Drawing.Point(150, 200);
+            this.tbCPU.Name = "tbCPU";
+            this.tbCPU.Size = new System.Drawing.Size(164, 20);
+            this.tbCPU.TabIndex = 12;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(28, 272);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(111, 38);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(212, 272);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(111, 38);
+            this.btnCancel.TabIndex = 14;
+            this.btnCancel.Text = "отменить";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Tablet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 339);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbCPU);
+            this.Controls.Add(this.tbStorage);
+            this.Controls.Add(this.tbMemory);
+            this.Controls.Add(this.tbOS);
+            this.Controls.Add(this.tbDiagonal);
+            this.Controls.Add(this.tbModel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Tablet";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Tablet_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbAdd;
+        private System.Windows.Forms.ToolStripButton tsbDelete;
+        private System.Windows.Forms.ToolStripButton tsbPrev;
+        private System.Windows.Forms.ToolStripButton tsbNext;
+        private System.Windows.Forms.ToolStripLabel tslCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbModel;
+        private System.Windows.Forms.TextBox tbDiagonal;
+        private System.Windows.Forms.TextBox tbOS;
+        private System.Windows.Forms.TextBox tbMemory;
+        private System.Windows.Forms.TextBox tbStorage;
+        private System.Windows.Forms.TextBox tbCPU;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ 9 лаба 2 часть/Tablet.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace _9_лаба_2_часть
+{
+    
+    public partial class Tablet : Form
+    {
+        private string model;
+        private double diagonal;
+        private string operationSystem;
+        private short memory;
+        private short storage;
+        private string cpu;
+        
+        //заполнение полей объекта значениями из строкового массива
+        public void SetStringValues(string[] array)
+        {
+            model = array[0];
+            diagonal = Convert.ToDouble(array[1]);
+            operationSystem = array[2];
+            memory = Convert.ToInt16(array[3]);
+            storage = Convert.ToInt16(array[4]);
+            cpu = array[5];
+        }
+        //получение строкового массива со значениями полей объекта
+        public string[] GetStringValues()
+        {
+            string[] array = new string[6]; //создаем массив
+
+            //заполняем его значениями
+            array[0] = model;
+            array[1] = diagonal.ToString();
+            array[2] = operationSystem;
+            array[3] = memory.ToString();
+            array[4] = storage.ToString();
+            array[5] = cpu;
+            return array; //возвращаем результат
+        }
+        private string filename = "tablets.dat"; 
+        private List<Tablet> Tablets = new List<Tablet>();
+        private int current = 0;
+        public Tablet()
+        {
+            InitializeComponent();
+        }
+        private void AddTablet()
+        {
+            Tablet buf = new Tablet(); 
+            string[] TabletInfo = new string[6];
+            TabletInfo[0] = tbModel.Text;
+            TabletInfo[1] = tbDiagonal.Text;
+            TabletInfo[2] = tbOS.Text;
+            TabletInfo[3] = tbMemory.Text;
+            TabletInfo[4] = tbStorage.Text;
+            TabletInfo[5] = tbCPU.Text;
+            buf.SetStringValues(TabletInfo); //заносим значения в объект
+            Tablets.Add(buf); //добавляем объект в список
+           
+        }
+        private void SaveFile()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            try
+            {
+                using (FileStream fs = new FileStream(filename,
+                FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    bf.Serialize(fs, Tablets); 
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Ошибка ввода-вывода: " + ex.ToString());
+            }
+            catch (SerializationException ex)
+            {
+                MessageBox.Show("Ошибка сериализации: " + ex.ToString());
+            }
+        }
+        private void Tablet_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsbAdd_Click(object sender, EventArgs e)
+        {
+            tbModel.Clear();
+            tbDiagonal.Clear();
+            tbOS.Clear();
+            tbMemory.Clear();
+            tbStorage.Clear();
+            tbCPU.Clear();
+            btnSave.Visible = true;
+            btnCancel.Visible = true;
+            Count();
+            
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            AddTablet(); 
+            SaveFile(); 
+            current = Tablets.Count - 1; 
+            btnSave.Visible = false;
+            btnCancel.Visible = false;
+            MessageBox.Show("Запись добавлена");
+            
+            
+        }
+        private void TabletToTextBoxes(int current)
+        {
+            if (Tablets.Count > 0)
+            {
+                string[] TabletInfo = Tablets[current].GetStringValues();
+                tbModel.Text = TabletInfo[0];
+                tbDiagonal.Text = TabletInfo[1];
+                tbOS.Text = TabletInfo[2];
+                tbMemory.Text = TabletInfo[3];
+                tbStorage.Text = TabletInfo[4];
+                tbCPU.Text = TabletInfo[5];
+            }
+            else 
+            {
+                tbModel.Clear();
+                tbDiagonal.Clear();
+                tbOS.Clear();
+                tbMemory.Clear();
+                tbStorage.Clear();
+                tbCPU.Clear();
+            }
+        }
+        private void ScrollTablet(sbyte inc)
+        {
+            
+            if ((current + inc <= Tablets.Count - 1) && (current + inc >= 0))
+            {
+                current += inc; 
+                TabletToTextBoxes(current); 
+            }
+        }
+
+        
+        //задание в самой программе 15 пункт
+        private void tsbPrev_Click(object sender, EventArgs e)
+        {
+
+            sbyte z = -1;
+            ScrollTablet(z);
+            TabletToTextBoxes(current);
+        }
+
+        private void tsbNext_Click(object sender, EventArgs e)
+        {
+            sbyte z=1;
+         ScrollTablet(z);
+            TabletToTextBoxes(current);
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            btnSave.Visible = false;
+            btnCancel.Visible = false;
+            TabletToTextBoxes(current);
+        }
+
+        private void DeleteTablet()
+        {
+            Tablets.RemoveAt(current); //удаляем значение из списка по его номеру
+            if (current > 0) //если удалялась не первая запись
+                current--; //делаем текущей предыдущую
+        }
+
+        private void tsbDelete_Click(object sender, EventArgs e)
+        {
+            if (Tablets.Count > 0) //если список не пуст
+            {
+                DeleteTablet(); //удаляем запись
+                                //если после удаления в списке не осталось значений 
+                if (Tablets.Count == 0)
+                {
+                    //очищаем текстовые поля
+                    tbModel.Clear();
+                    tbDiagonal.Clear();
+                    tbOS.Clear();
+                    tbMemory.Clear();
+                    tbStorage.Clear();
+                    tbCPU.Clear();
+                }
+                else
+                {
+                    TabletToTextBoxes(current); //выводим информацию о текущей записи
+                }
+                SaveFile(); //сохраняем изменения 
+                MessageBox.Show("Запись удалена");
+            }
+        }
+
+       
+        private void tslCount_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void tslCount_VisibleChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void tslCount_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void tslCount_LocationChanged(object sender, EventArgs e)
+        {
+           
+        }
+        //задание 2
+        private void Count()
+        {
+            int have = 0;
+            string content;
+            int sum = 0;
+            if (Tablets.Count > 0)
+            {
+                sum = have + 1;
+                content = "Кол-во записей: " + sum;
+            }
+            else
+            {
+                content = "Нет записей для отображения";
+            }
+            tslCount.Text = content;
+
+        }
+    }
+}
